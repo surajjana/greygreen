169671b3a7736850b1a999576353b48947ced97b
Suraj Kumar Jana <surajjana2@gmail.com>
2016-10-24 15:51:47 +0530

    some commit

--- server.py
+++ server.py
@@ -65,7 +65,74 @@ def data():
 
 @app.route('/json_data')
 def json_data():
-	cur = db.greygreen.find({"r_id": 0})
+	cur = db.greygreen.find()
 	data = json.loads(dumps(cur))
 
-	return json.dumps(data)
+	res = [{'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 0'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 1'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 2'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 3'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 4'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 5'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 6'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 7'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 8'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 9'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 10'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 11'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 12'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 13'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 14'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 15'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 16'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 17'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 18'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 19'}, {'x': [], 'y': [], 'type': 'scatter', 'name': 'Restaurant 20'}]
+
+	for i in range(0, len(data)):
+		if(data[i]['r_id'] == 0):
+			res[0]['x'].append(data[i]['time'])
+			res[0]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 1):
+			res[1]['x'].append(data[i]['time'])
+			res[1]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 2):
+			res[2]['x'].append(data[i]['time'])
+			res[2]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 3):
+			res[3]['x'].append(data[i]['time'])
+			res[3]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 4):
+			res[4]['x'].append(data[i]['time'])
+			res[4]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 5):
+			res[5]['x'].append(data[i]['time'])
+			res[5]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 6):
+			res[6]['x'].append(data[i]['time'])
+			res[6]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 7):
+			res[7]['x'].append(data[i]['time'])
+			res[7]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 8):
+			res[8]['x'].append(data[i]['time'])
+			res[8]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 9):
+			res[9]['x'].append(data[i]['time'])
+			res[9]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 10):
+			res[10]['x'].append(data[i]['time'])
+			res[10]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 11):
+			res[11]['x'].append(data[i]['time'])
+			res[11]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 12):
+			res[12]['x'].append(data[i]['time'])
+			res[12]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 13):
+			res[13]['x'].append(data[i]['time'])
+			res[13]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 14):
+			res[14]['x'].append(data[i]['time'])
+			res[14]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 15):
+			res[15]['x'].append(data[i]['time'])
+			res[15]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 16):
+			res[16]['x'].append(data[i]['time'])
+			res[16]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 17):
+			res[17]['x'].append(data[i]['time'])
+			res[17]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 18):
+			res[18]['x'].append(data[i]['time'])
+			res[18]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 19):
+			res[19]['x'].append(data[i]['time'])
+			res[19]['y'].append(data[i]['rating'])
+		elif(data[i]['r_id'] == 20):
+			res[20]['x'].append(data[i]['time'])
+			res[20]['y'].append(data[i]['rating'])
+
+	return json.dumps(res)
--- templates/index.tpl
+++ templates/index.tpl
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Scatter Time Series</title>
+    <title>Grey Green | Restaurant Data</title>
     <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 </head>
 
 
@@ -10,16 +11,17 @@
   
   <div id="myDiv" style="width: 480px; height: 400px;"><!-- Plotly chart will be drawn inside this DIV --></div>
   <script>
-    var data = [
-      {
-        x: ['2016-10-24 13:00:00', '2016-10-24 13:30:00', '2016-10-24 14:2:41'],
-        y: [1, 3, 4],
-        type: 'scatter',
-        name: 'Restaurant 1'
-      }
-    ];
+    $(document).ready(function(){
+      $.getJSON('http://127.0.0.1:8000/json_data', function(jd) {
 
-    Plotly.newPlot('myDiv', data);
+        console.log(jd);
+
+        var data = jd;
+
+        Plotly.newPlot('myDiv', data);
+                  
+      });
+    });
   </script>
 </body>
 
